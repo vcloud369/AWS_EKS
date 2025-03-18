def644c9cffe90b319d7340fa6e8a9ef76ae9f82
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-18 13:49:13 +0530

    Update variables.tf

--- infra/Modules/IAM/variables.tf
+++ infra/Modules/IAM/variables.tf
@@ -1,5 +1,5 @@
 variable "eks_node_role_name" {
   description = "Name of the IAM role for EKS Node group"
   type        = string
-  default     = "eks-node-role"
+  default     = "eks-node-role1"
 }
