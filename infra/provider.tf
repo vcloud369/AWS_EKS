def2ab5f76e8b646d7878b56266850ac6be31f81
Nisev@789 <vcloud369@gmail.com>
2025-10-03 18:47:46 +0530

    K8s config path updated

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,6 +8,14 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
+
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
